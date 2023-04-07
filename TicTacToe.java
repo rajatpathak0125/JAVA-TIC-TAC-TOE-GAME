625e6f692f26c3cd5b000532ae7737f0939ba9e6
rajat-pathk <100964436+rajat-pathk@users.noreply.github.com>
2023-04-07 22:57:49 +0530

    Add files via upload
    This is the program of tic tac toe game in java take takes input from player X and player O int the form of 3d array coordinates and declares the winner when the game is over.

---
+++ TicTacToe.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (char[] chars : board) {
+            Arrays.fill(chars, ' ');
+        }
+
+        char player = 'X';
+        boolean gameOver = false;
+        Scanner scanner = new Scanner(System.in);
+
+        while (!gameOver) {
+            printBoard(board);
+            System.out.print("Player " + player + " enter: ");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+            System.out.println();
+
+            if (board[row][col] == ' ') {
+                board[row][col] = player; // place the element
+                gameOver = haveWon(board, player);
+                if (gameOver) {
+                    System.out.println("Player " + player + " has won: ");
+                } else {
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+            } else {
+                System.out.println("Invalid move. Try again!");
+            }
+        }
+        printBoard(board);
+    }
+
+    public static boolean haveWon(char[][] board, char player) {
+        // check the rows
+        for (char[] chars : board) {
+            if (chars[0] == player && chars[1] == player && chars[2] == player) {
+                return true;
+            }
+        }
+
+        // check for col
+        for (int col = 0; col < board[0].length; col++) {
+            if (board[0][col] == player && board[1][col] == player && board[2][col] == player) {
+                return true;
+            }
+        }
+
+        // diagonal
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            return true;
+        }
+
+        return board[0][2] == player && board[1][1] == player && board[2][0] == player;
+    }
+
+    public static void printBoard(char[][] board) {
+        for (char[] chars : board) {
+            for (char aChar : chars) {
+                System.out.print(aChar + " | ");
+            }
+            System.out.println();
+        }
+    }
+}
